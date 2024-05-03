898cd3caeacbc9d25a8ec60f5725b3d2318575d1
steveTech153 <shawlingtemplesteve@gmail.com>
2024-05-03 14:33:38 +0530

    Cleared unwanted function

--- src/Main.java
+++ src/Main.java
@@ -5,39 +5,6 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
-    public void dummy() {
-//        Admin admin = new Admin("admin");
-//        admin.setCities(new ArrayList<String>(Arrays.asList("Chennai", "Madurai", "Trichy", "Coimbatore", "Salem", "Tirunelveli", "Tiruppur", "Erode", "Vellore", "Tiruvannamalai")));
-//
-//        Booker booker = new Booker();
-//        User user1 = new User("user1");
-//        int choice = 1;
-//
-//        booker.showAvailableNoOfSeats("Chennai", "Madurai", "2024-04-29");
-//
-//        booker.book(user1, "Chennai", "Madurai", "2024-04-29", 19, false);
-//        user1.viewBookings();
-//
-//        User user2 = new User("user2");
-//        booker.book(user2, "Chennai", "Madurai", "2024-04-29", 1, false);
-//        user2.viewBookings();
-//
-//        User user3 = new User("user3");
-//        booker.book(user3, "Chennai", "Madurai", "2024-04-29", 5, false);
-//        user3.viewBookings();
-//
-//        booker.cancelBooking(user2,2, false);
-//
-//        user3.getBooking(3).viewDetails();
-//
-//        booker.requestChangeDate(user1, 1, "2024-04-30");
-////        booker.showAvailableNoOfSeats("Chennai", "Madurai", "2024-04-30");
-//
-//        user3.getBooking(3).viewDetails();
-//
-//        booker.showAvailableNoOfSeats("Chennai", "Madurai", "2024-04-29");
-    }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Bus Booking System\n");
